948262ddd04165ee7bfaefdec75d394de27fb812
Gabriele Cipolloni <cipolloni204@gmail.com>
2025-02-21 12:35:14 +0100

    first comm

---
+++ src/main/java/com/example/BES6L5ESE/Bes6l5eseApplication.java
@@ -0,0 +1,13 @@
+package com.example.BES6L5ESE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bes6l5eseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bes6l5eseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BES6L5ESE/Bes6l5eseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BES6L5ESE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bes6l5eseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
